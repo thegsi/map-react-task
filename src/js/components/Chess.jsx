8bc22da22a5de2442159cf549e54385f9d703cf3
thegsi <thegsi@hotmail.com>
2016-02-18 13:03:42 +0000

    web pack and hapi working

---
+++ src/js/components/Chess.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Chess = React.createClass({
+
+  render: function() {
+    return <div>
+      <p>Gethi</p>
+    </div>;
+    <div><img src="http://i.stack.imgur.com/fpgJx.jpg" /></div>
+  }
+});
+
+module.exports = Chess;
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -2,23 +2,34 @@ var webpack = require('webpack');
 
 module.exports = {
 
-    entry: [
-      'webpack/hot/dev-server',
-      "./src/js/main.jsx"
-    ],
-    output: {
-      path: __dirname + '/public',
-      filename: "bundle.js",
-      publicPath: "/public/"
-    },
-    module: {
-      loaders: [
-        { test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot','babel-loader']},
-        { test: /\.css$/, loader: 'style!css'}
-      ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
+  entry: [
+    'webpack/hot/dev-server',
+    "./src/js/main.jsx"
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: "bundle.js",
+    publicPath: "/public/"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }, {
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'react-hot'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
 
 };
