4da6177678eb7c3e7ab646bc85dd4cb24778c0ab
thegsi <thegsi@hotmail.com>
2016-02-22 15:56:23 +0000

    board re rendering with state change

--- test/index.jsx
+++ test/index.jsx
@@ -1,9 +1,2 @@
-// require('./app.test.jsx');
-require('./select-airport.test.jsx');
+require('./square.test.jsx');
 require('./hapi-test.js');
-require('./header.test.jsx');
-require('./select-flight.test.jsx');
-// require('./food-item.test.jsx');
-// require('./food-type.test.jsx');
-// require('./menu.test.jsx');
-// require('./restaurant-header.test.jsx');
